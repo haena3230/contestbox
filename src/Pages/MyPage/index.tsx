eff6655c6f648d0165f4f353a7a07db2841d7af1
haena3230 <gosk0912@gmail.com>
2021-05-31 01:14:53 +0900

    Add MyPage

--- src/Pages/MyPage/index.tsx
+++ src/Pages/MyPage/index.tsx
@@ -1,13 +1,47 @@
 // my page
 import React from 'react'
 import { Text, View } from 'react-native'
+import styled from 'styled-components/native'
+import {Color, IconSize, Styles} from '~/Styles'
+// component
+import Header from '~/Components/Header'
+import { Container } from '~/Styles'
+// icon
+import UserIcon from '~/Assets/assignment_ind_black_24dp.svg'
+import StarIcon from '~/Assets/star_outline_black_24dp.svg'
+import PenIcon from '~/Assets/create_black_24dp.svg';
 
 const MyPage= ()=>{
     return(
-        <View>
-            <Text>my</Text>
+        <View style={{flex:1}}>
+            <Header />
+            <Container style={{justifyContent:'center'}}>
+                <BoxContainer>
+                    <UserIcon style={{marginRight:10}} height={IconSize.bicon} width={IconSize.bicon} fill={Color.gray} />
+                    <Text style={Styles.m_m_font}>개인정보 변경</Text>
+                </BoxContainer>
+                <BoxContainer>
+                    <StarIcon style={{marginRight:10}} height={IconSize.bicon} width={IconSize.bicon} fill={Color.gray} />
+                    <Text style={Styles.m_m_font}>스크랩 목록</Text>
+                </BoxContainer>
+                <BoxContainer>
+                    <PenIcon style={{marginRight:10}} height={IconSize.bicon} width={IconSize.bicon} fill={Color.gray} />
+                    <Text style={Styles.m_m_font}>내가 쓴 글</Text>
+                </BoxContainer>            
+            </Container>    
         </View>
     )
 }
 
 export default MyPage
+
+const BoxContainer = styled.TouchableOpacity`
+    background-color:${Color.artbox}
+    border-width:1px;
+    border-color:${Color.border};
+    border-radius:5px;
+    padding:10px;
+    margin-vertical:5px;
+    flex-direction:row;
+    align-items:center;
+`
