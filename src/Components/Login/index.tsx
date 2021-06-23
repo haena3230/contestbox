cf9fe5f73fd56009a16d906b555c41b5a9b4b9f8
haena3230 <gosk0912@gmail.com>
2021-06-24 01:09:32 +0900

    ck merge state

--- src/Components/Login/index.tsx
+++ src/Components/Login/index.tsx
@@ -3,8 +3,6 @@
 // 로그인
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
-import { useState } from 'react';
-
 
 export const SignIn = async () => {
 
@@ -37,23 +35,3 @@ export const SignOut = async () => {
     console.error(error);
   }
 };
-
-
-// export const EmailSignIn=()=>{
-//   auth()
-//   .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-//   .then(() => {
-//     console.log('User account created & signed in!');
-//   })
-//   .catch(error => {
-//     if (error.code === 'auth/email-already-in-use') {
-//       console.log('That email address is already in use!');
-//     }
-
-//     if (error.code === 'auth/invalid-email') {
-//       console.log('That email address is invalid!');
-//     }
-
-//     console.error(error);
-//   })
-// }
